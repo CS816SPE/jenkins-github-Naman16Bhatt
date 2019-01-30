2eb2f1cbd869bfeb8e075c60aa86ec3b978408e3
Naman16Bhatt <naman.bhatt@iiitb.org>
2019-01-30 22:34:22 +0530

    Assignment 1 part 3

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*; 
+class Branch{
+    public static void main(String ar[]){
+    
+       System.out.println("Hello friends"); 
+System.out.println("Hello friends"); 
+    }
+  
+}
